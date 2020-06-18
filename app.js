d6a82e14c54017da66a1fe7c02b1596910a42dc4
Avivtc <avivtc@gmail.com>
2020-06-18 20:21:52 +0300

    add files

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+
+  
+
+
+      function sumNumber(n){
+        var total = 0;
+        for(var i = 1; i <= n; i++){
+          total += i;
+        }
+        return alert(total)
+      }
+
+      sumNumber(3);
